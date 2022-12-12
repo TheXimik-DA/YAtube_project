522c84063412e239288077141fdc62c4fa571b32
Гаджи Шахбанов <mister.tool@mail.ru>
2022-12-12 21:02:20 +0300

    «Добавлены urls и view-функции».

---
+++ yatube/posts/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком мороженого
+def group_posts(request, slug):
+    return HttpResponse('По группам фильтруем')
+    
